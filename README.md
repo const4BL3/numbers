42926c1c34003b5322d42699c603588a4b3e255e
Clinton Billedeaux <cbilledeaux@watchsystems.com>
2021-11-24 10:12:37 -0600

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rounding Errors
+
+Why did we build computers with two voltage points when we generally use base 10 for math?
+Actually, with 1 and 0, there's enough problems to deal with. I can't imagine what would happen if we gave computers 10 different voltages to deal with.
---
+++ numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// There is a thing about double.
+
+// Setup
+double doubleValue = 0.3;
+double myDouble = 0;
+const int iterator = 10000000;
+
+
+// Addition
+for (int i = 0; i < iterator; i++)
+{
+    myDouble += doubleValue;
+}
+
+Console.WriteLine("Added: " + myDouble);
+
+// Reset
+doubleValue = 0.3;
+myDouble = 0;
+
+// Multiplication
+myDouble = doubleValue * iterator;
+
+Console.WriteLine("Multiplied: " + myDouble);
+Console.WriteLine("See the following on StackOverflow: https://stackoverflow.com/questions/3730019/why-not-use-double-or-float-to-represent-currency");
